bf1d8fc1dbe9a46a8277cb2791619f82ca0e75a6
ActiveCG <sivers12@student.aau.dk>
2016-09-27 13:13:54 +0200

    scene manager, player winning

---
+++ LostPigglets/Assets/Scripts/PickUp.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUp : MonoBehaviour {
+	public static int piggletsCollected = 0;
+
+	//when player picks up something
+	void OnTriggerEnter(Collider other){
+		//picking up pigglets
+		if (other.gameObject.tag == "Pigglet") {
+			Debug.Log ("pigglet");
+			piggletsCollected++;
+			//make pigglets follow pig
+			other.gameObject.transform.rotation = transform.rotation;
+			other.gameObject.transform.position = transform.position + new Vector3 (0f, 0f, -1.5f * piggletsCollected);
+			other.gameObject.transform.parent = transform;
+
+			//check whther all pigglets collected
+			if (piggletsCollected == PlayerStats.playerStatsCurrent.piggletsInGame) {
+				SceneController.current.Win ();
+			}
+		}
+	}
+}
---
+++ LostPigglets/Assets/Scripts/SceneController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+	public static SceneController current;
+
+	void Start(){
+		current = this;
+	}
+
+	//to happen when player wins
+	public void Win(){
+		SceneManager.LoadScene ("Win");
+	}
+
+	//to happen when player dies
+	public void GameOver(){
+		SceneManager.LoadScene ("GameOver");
+	}
+}
