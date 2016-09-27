90a0fb94d45e669f2d5c082f449f20a3ed79d4a7
Alexandros <giakalis87@hotmail.com>
2016-09-27 14:25:42 +0200

    Merged with the master

--- LostPigglets/Assets/Scripts/MoveEnemies.cs
+++ LostPigglets/Assets/Scripts/MoveEnemies.cs
@@ -4,7 +4,14 @@ using System.Collections;
 public class MoveEnemies : MonoBehaviour {
 
     private NavMeshAgent nav;
+
+    [HideInInspector]
     public Transform target;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
 
     void Update () {
         nav = GetComponent<NavMeshAgent>();
