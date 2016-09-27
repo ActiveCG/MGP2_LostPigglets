2060343628337d4241299b0829db1d6c52835f03
ActiveCG <sivers12@student.aau.dk>
2016-09-27 14:46:17 +0200

    monster attacks and player dies

---
+++ LostPigglets/Assets/Scripts/MonsterAttack.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterAttack : MonoBehaviour {
+	public delegate void MonsterAttacks();
+	public static event MonsterAttacks monsterAttacks;
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "Player") {
+			Attack ();
+		}
+	}
+
+	//attack player
+	void Attack(){
+		monsterAttacks ();
+	}
+}
---
+++ LostPigglets/Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerLife : MonoBehaviour {
+
+	void OnEnable () {
+		MonsterAttack.monsterAttacks += TakeDamage;
+	}
+	
+	void OnDisable() {
+		MonsterAttack.monsterAttacks -= TakeDamage;
+	}
+
+	//recieve damage from monsters
+	void TakeDamage(){
+		Die ();
+	}
+
+	void Die(){
+		SceneController.current.GameOver ();
+	}
+}
