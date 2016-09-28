ce03b04a47811c587df7d48183cc24c8ed321726
Esben Gravesen <esben.gravesen@gmail.com>
2016-09-28 09:27:19 +0200

    Temp sounds

--- LostPigglets/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ LostPigglets/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,7 +13,11 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID TEST_EVENT = 4258653052U;
+        static const AkUniqueID MONSTER_SWIM_START = 1632505953U;
+        static const AkUniqueID MONSTER_SWIM_STOP = 3461698459U;
+        static const AkUniqueID PIG_SWIM_START = 1548288955U;
+        static const AkUniqueID PIG_SWIM_STOP = 1750874649U;
+        static const AkUniqueID PIGLET_OINK = 4036007832U;
     } // namespace EVENTS
 
     namespace GAME_PARAMETERS
@@ -38,9 +42,18 @@ namespace AK
 
     namespace BUSSES
     {
+        static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID ENVIRONMENTAL = 1973600711U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
         static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+        static const AkUniqueID SFX = 393239870U;
+        static const AkUniqueID UI = 1551306167U;
     } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID REVERB = 348963605U;
+    } // namespace AUX_BUSSES
 
 }// namespace AK
 
