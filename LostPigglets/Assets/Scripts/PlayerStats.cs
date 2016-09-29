56a7284e8fb329ea8ce36dfe02e4e6caa7792039
Mikkel Mulvad Beyer <piercefield1992@gmail.com>
2016-09-29 13:45:30 +0200

    Merging the MVP level with the Development Scene

--- LostPigglets/Assets/Scripts/PlayerStats.cs
+++ LostPigglets/Assets/Scripts/PlayerStats.cs
@@ -5,9 +5,6 @@ public class PlayerStats : MonoBehaviour {
 
 	public static PlayerStats playerStatsCurrent;
 
-	public float playerHealth;
-	public float playerSpeed;
-
 	public int piggletsInGame; //number of pigglets in the level
 
 	void Awake()
