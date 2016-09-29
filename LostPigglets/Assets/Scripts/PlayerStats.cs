9959029f3f9ab9a723677486e711ec76b1b65e86
ActiveCG <sivers12@student.aau.dk>
2016-09-29 14:10:29 +0200

    player health

--- LostPigglets/Assets/Scripts/PlayerStats.cs
+++ LostPigglets/Assets/Scripts/PlayerStats.cs
@@ -5,6 +5,8 @@ public class PlayerStats : MonoBehaviour {
 
 	public static PlayerStats instance;
 
+	public int playerHealth;
+
 	public int piggletsInGame; //number of pigglets in the level
 
 	void Awake()
