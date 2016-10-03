ba0f42de731ae1beb6af18dd418a18c110ff41ba
Mikkel Mulvad Beyer <piercefield1992@gmail.com>
2016-10-03 16:37:19 +0200

    Added Pig Charge Water Splash particle effect, added scrolling water

---
+++ LostPigglets/Assets/Scripts/WaterScroll.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterScroll : MonoBehaviour {
+
+	public float scrollSpeed = 0.5F;
+	public float offsetX, offsetY;
+	public Renderer rend;
+	void Start() {
+		rend = GetComponent<Renderer>();
+	}
+	void Update() {
+		rend.material.SetTextureOffset("_MainTex", new Vector2(scrollSpeed *Mathf.Sin(Time.time *  offsetX), scrollSpeed *Mathf.Sin(Time.time *  offsetY)));
+	}
+}
