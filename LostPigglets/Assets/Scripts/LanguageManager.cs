38fed2f71418c8a3c743fec437bded342892e167
Christian Andersen <s113990@student.dtu.dk>
2016-10-03 16:31:04 +0200

    Main Menu implemented

---
+++ LostPigglets/Assets/Scripts/LanguageManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LanguageManager {
+
+    private static LanguageManager _instance;
+
+    public string danishNewGame = "NYT SPIL";
+
+    public string englishNewGame = "NEW GAME";
+    public string englishQuitGame = "QUIT GAME";
+    public string englishTitle = "JOURNEY THROUGH THE MIDNIGHT FOREST";
+    public string englishSubTitle = "LOST PIGLETS";
+
+    //getters:
+    public static LanguageManager instance {
+        get {
+            if (_instance == null)
+                _instance = new LanguageManager();
+            return _instance;
+        }
+    }
+}
---
+++ LostPigglets/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class MenuManager : MonoBehaviour {
+
+    GameObject playGame;
+    GameObject quitGame;
+    GameObject title;
+    GameObject subTitle;
+
+	// Use this for initialization
+	void Start () {
+
+        playGame = GameObject.FindGameObjectWithTag("PlayGameMenu");
+        quitGame = GameObject.FindGameObjectWithTag("QuitGameMenu");
+        title = GameObject.FindGameObjectWithTag("Title");
+        subTitle = GameObject.FindGameObjectWithTag("SubTitle");
+
+        playGame.GetComponentInChildren<Text>().text = LanguageManager.instance.englishNewGame;
+        quitGame.GetComponentInChildren<Text>().text = LanguageManager.instance.englishQuitGame;
+        title.GetComponentInChildren<Text>().text = LanguageManager.instance.englishTitle;
+        subTitle.GetComponentInChildren<Text>().text = LanguageManager.instance.englishSubTitle;
+
+    }
+
+    public void PlayGame() {
+                
+        playGame.transform.localScale = new Vector3(1.2f,1.2f,1.2f);
+        StartCoroutine(Delay());
+        SceneManager.LoadScene("Development");
+    }
+
+    IEnumerator Delay() {
+        yield return new WaitForSeconds(0.2f);
+    }
+
+    public void QuitGame() {
+        quitGame.transform.localScale = new Vector3(1.2f, 1.2f, 1.2f);
+        StartCoroutine(Delay());
+        Application.Quit();
+    }
+}
