3be82950d2586f9dadfab3cfd1cc94d1fd19c240
AlexanderBjorkholm <alexander_bjorkholm@hotmail.com>
2016-10-05 00:14:33 +0200

    Art into developmentScene

--- LostPigglets/Assets/Scripts/MonsterStun.cs
+++ LostPigglets/Assets/Scripts/MonsterStun.cs
@@ -52,13 +52,11 @@ public class MonsterStun : MonoBehaviour
         else
         {
             print("I am on this fucking stun");
-            particleStunned = GameObject.FindGameObjectWithTag("Enemy").GetComponent<ParticleSystem>();
-            particleStunned.Play();
+            //particleStunned = GameObject.FindGameObjectWithTag("Enemy").GetComponent<ParticleSystem>();
+            //particleStunned.Play();
             GameManager.instance.monsterNotMoving(obj); //monster stops swimming
             GameManager.instance.MonsterStun(obj);
             monsterStunned = true;
-            //particleStunned = GameObject.FindGameObjectWithTag("Enemy").GetComponent<ParticleSystem>();
-            //particleStunned.Play();
 			particleStunned.SetActive(true);
             PlayerStats.instance.spotlight.intensity = 8;
             print(PlayerStats.instance.spotlight.intensity);
